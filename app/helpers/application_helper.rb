7840bac60730b023d1731bb05519ca6e2620cce4
Richie Khoo <evolve2k@gmail.com>
2009-09-04 16:48:47 +0930

    Implemented Gravatar Support and formatted Volunteer views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,20 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
 
+  require 'digest/md5'
+ 
   def current_controller?(c)
     controller.controller_name == c
+  end
+  
+  def gravatar_url_for(email, options = {})   # 'Gravatars on Rails' sourced from http://overhrd.com/?p=28
+    url_for({ 
+      :gravatar_id => Digest::MD5.hexdigest(email), 
+      :host => 'www.gravatar.com',
+	  :protocol => 'http://',
+	  :only_path => false,
+	  :controller => 'avatar.php'
+    }.merge(options))  
   end
 
 end
