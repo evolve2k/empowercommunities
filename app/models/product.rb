635c0c9dbf47226cb85d1a57d4143cc4b1897832
Richie Khoo <evolve2k@gmail.com>
2009-09-21 14:11:00 +0930

    small changes

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,4 @@
 class Product < ActiveRecord::Base
-  has_many :area_product, :class_name => 'Product'
+  has_many :area_products, :class_name => 'Product'
   accepts_nested_attributes_for :area_product, :allow_destroy => true, :reject_if => :all_blank
 end
