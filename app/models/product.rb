6aa953f8239687c858bff1b58baf40a413e5bd93
Tyson Avery <tysonaverydfc@gmail.com>
2009-09-25 13:28:46 +0930

    merged with Richies branch

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,10 @@
 class Product < ActiveRecord::Base
+
  validates_inclusion_of :first_production,
     :in => Date.civil(2004,1,1) .. Date.today,
 	:message => "Must be a valid production date!"
+
+  has_many :area_products
+  accepts_nested_attributes_for :area_products, :allow_destroy => true, :reject_if => proc { |attrs| attrs.all? { |k, v| v.blank? } }
 
 end
